5a2543016d708eaee2abac46207bc5512c12325c
prathu1846 <159513648+prathu1846@users.noreply.github.com>
2024-03-14 19:01:34 +0530

    Add files via upload

---
+++ js.lec2/script.js
@@ -0,0 +1,10 @@
+//Create a const object called Product to store information shown inn the picture
+
+const product = {
+    title : "Ball Pen",
+    rating : 4,
+    offer : 5,
+    price : 270,
+};
+
+console.log(product);
