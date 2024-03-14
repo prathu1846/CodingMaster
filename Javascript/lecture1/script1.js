97befef820c1c6c248d5c0c33b7305d0ba493caa
prathu1846 <159513648+prathu1846@users.noreply.github.com>
2024-03-14 17:36:13 +0530

    Add files via upload

---
+++ Javascript/lecture1/script1.js
@@ -0,0 +1,23 @@
+// Assignment Operator 
+
+// let a = 5;
+// let b = 4;
+
+// a += 4;
+// console.log("a = " , a);
+
+
+// Comparison Operator
+
+// let a = 4;
+// let b = 5;
+
+// console.log(" 4>5", a>b);
+
+
+//Logical Operator
+
+let a =7;
+let b =6;
+
+console.log("cond1 || cond2 = ", a<b || a == 7);
