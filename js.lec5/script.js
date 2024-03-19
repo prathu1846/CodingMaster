9eb20dc5f24654b037ea25b344bdea4845b914ca
Prathamesh Farade <159513648+prathu1846@users.noreply.github.com>
2024-03-19 09:26:29 +0530

    Add files via upload

---
+++ js.lec5/script.js
@@ -0,0 +1,15 @@
+// Create an array to store companies "Blooming,"Microsoft","Uber","Google","Netflix" 
+
+// Remove the first company from the array 
+
+let companies = ["Blooming","Microsoft","Uber","Google","Netflix"];
+
+//companies.shift();
+
+//Remove Uber & ass ola in its place
+
+//companies.splice(2,1,"Ola");
+
+//Add Amazon at the end
+
+companies.push("Amazon"); // using to push method in aaray format.
